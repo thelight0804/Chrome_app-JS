4f0fc09851b66ad3b25a82b568c5d44e4e6232e7
Thelight0804 <69424845+thelight0804@users.noreply.github.com>
2021-08-13 17:16:26 +0900

    Hide API_Key

--- JS/06.weather.js
+++ JS/06.weather.js
@@ -1,5 +1,5 @@
 //21.08.13 / 날씨 추가
-const API_KEY = "837edfbcf40440e7adadebc03d9182db"; //openweather API Key
+const API_KEY = "###"; //openweather API Key
 
 
 function GeoOk(position){ //(position) : JS가 기본으로 제공하는 좌표 위치
